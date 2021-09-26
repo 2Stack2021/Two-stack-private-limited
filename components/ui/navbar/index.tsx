3c6a60cd6aab4305d1e0e12d989e35f9f92f5ec1
2Stack2021 <2twostack@gmail.com>
2021-09-26 11:25:27 +0530

    make it better

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -1,5 +1,48 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "../../../public/logo/favicon.png";
 
 export const NavBar: React.FC = () => {
-     return <div>Navbar</div>;
+     return (
+          <nav className="px-2 bg-white sticky top-0 z-50 w-full shadow-2xl">
+               <div className="container mx-auto flex flex-row justify-between items-center">
+                    <div className="relative h-20 w-20 mt-2">
+                         <Image src={Logo} alt="Two Stack Private Limited" />
+                    </div>
+                    <ul className="lg:flex hidden lg:flex-row lg:items-center lg:justify-end lg:w-full">
+                         <a href="#home">
+                              <li className="text-primary-500 text-md mr-5 uppercase font-semibold">
+                                   Home
+                              </li>
+                         </a>
+                         <a href="#techs">
+                              <li className="text-primary-500 text-md mr-5 uppercase font-semibold">
+                                   Tech stack
+                              </li>
+                         </a>
+                         <a href="#technologies">
+                              <li className="text-primary-500 text-md mr-5 uppercase font-semibold">
+                                   Technologies
+                              </li>
+                         </a>
+                         <a href="#Indus">
+                              <li className="text-primary-500 text-md mr-5 uppercase font-semibold">
+                                   Industries
+                              </li>
+                         </a>
+
+                         <a href="#shadow">
+                              <li className="text-primary-500 text-md mr-5 uppercase font-semibold">
+                                   Showcase
+                              </li>
+                         </a>
+                         <a href="#contact">
+                              <li className="text-primary-500 text-md rounded-md uppercase bg-primary-100 p-2">
+                                   Contact us
+                              </li>
+                         </a>
+                    </ul>
+               </div>
+          </nav>
+     );
 };
