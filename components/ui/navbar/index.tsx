ec5d83f5e689fdf8cc5f3be023dd8ce60aa46a6f
2Stack2021 <2twostack@gmail.com>
2021-09-26 11:50:41 +0530

    make it better

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Image from "next/image";
-import Logo from "../../../public/logo/favicon.png";
+import { INavbarProps } from "../../../types";
 
-export const NavBar: React.FC = () => {
+export const NavBar: React.FC<INavbarProps> = ({ links, logo }) => {
      return (
           <nav className="px-2 bg-white sticky top-0 z-50 w-full shadow-2xl">
                <div className="container mx-auto flex flex-row justify-between items-center">
                     <div className="relative h-20 w-20 mt-2">
-                         <Image src={Logo} alt="Two Stack Private Limited" />
+                         <Image src={logo} alt="Two Stack Private Limited" />
                     </div>
                     <ul className="lg:flex hidden lg:flex-row lg:items-center lg:justify-end lg:w-full">
+                         {links.map(({ id, name, title }) => (
+                              <a href={id} key={name}>
+                                   <li className="text-primary-500 text-md mr-5 uppercase font-semibold">
+                                        {title}
+                                   </li>
+                              </a>
+                         ))}
                          <a href="#home">
                               <li className="text-primary-500 text-md mr-5 uppercase font-semibold">
                                    Home
